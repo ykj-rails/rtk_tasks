10e658366f41bea332af1e0f709d83f34b7d0f32
Yuki M <yukiji.0307@gmail.com>
2021-11-05 08:25:48 +0900

    [update] App.js

--- src/App.js
+++ src/App.js
@@ -1,58 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from 'react'
+import styles from './App.module.css'
+import { FaSignInAlt } from 'react-icons/fa'
 
 function App() {
+  const Logout = () => {
+    localStorage.removeItem('localJWT')
+    window.location.href = '/'
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className={styles.containerTasks}>
+      <div className={styles.appTasks}>
+        <button onClick={Logout} className={styles.signBtn}>
+          <FaSignInAlt />
+        </button>
+      </div>
+      <div className={styles.appDetails}></div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
