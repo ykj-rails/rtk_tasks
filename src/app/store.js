03f924f9f68934dd2c64118b3bd8ebd3bd1fbd03
Yuki M <yukiji.0307@gmail.com>
2021-11-07 15:22:15 +0900

    色々整理

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from '../features/counter/counterSlice'
 import loginReducer from '../features/login/loginSlice'
 import taskReducer from '../features/task/taskSlice'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     login: loginReducer,
     task: taskReducer,
   },
